b4a5ee533a7b49f0052f3b2cdd8bb24d732b3663
maxfil333_BT <maxfil333@gmail.com>
2024-09-09 13:21:27 +0300

    pdf funcs and folder_former

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,16 @@
 import os
 import fitz
+import json
 import base64
 import shutil
 import PyPDF2
 import numpy as np
 from PIL import Image
 from io import BytesIO
+from typing import Optional
 
 from logger import logger
+from config import config
 
 
 # _____ FOLDERS _____
@@ -18,6 +21,37 @@ def delete_all_files(dir_path: str):
             shutil.rmtree(folder_.path)
         else:
             os.remove(folder_.path)
+
+
+def folder_former(json_string: str, original_file: str, out_path: str) -> None:
+    # ___ generate document name ___
+    dct = json.loads(json_string)
+    doc_type = dct['Тип документа']
+    doc_number = dct['Номер документа']
+    doc_conos = dct['Номер коносамента']
+    transactions = dct['Номера таможенных сделок']
+    if doc_type == 'коносамент':
+        if not doc_conos:
+            doc_conos = 'untitled_conos'
+        new_name = doc_conos + os.path.splitext(original_file)[-1]
+    else:
+        if not doc_number:
+            transliteration = {'акт': 'act', 'коносамент': 'conos', 'заключение': 'report', 'протокол': 'protocol'}
+            doc_number = f'untitled_{transliteration[doc_type]}'
+        new_name = doc_number + os.path.splitext(original_file)[-1]
+
+    # ___ distribute into folders ___
+    if not transactions:
+        target_dir: str = config['untitled']
+        new_path = get_unique_filename(os.path.join(target_dir, new_name))
+        shutil.copy(original_file, new_path)
+    else:
+        for transaction in transactions:
+            target_dir = os.path.join(out_path, transaction)
+            if not os.path.isdir(target_dir):
+                os.makedirs(target_dir, exist_ok=False)
+            new_path = get_unique_filename(os.path.join(target_dir, new_name))
+            shutil.copy(original_file, new_path)
 
 
 # _____ COMMON _____
@@ -56,19 +90,67 @@ def switch_to_latin(s: str, reverse: bool = False) -> str:
 
 # _____ PDF _____
 
-def extract_text_with_fitz(pdf_path, only_first_page: bool = False):
-    document = fitz.open(pdf_path)
+def count_pages(file_path):
+    try:
+        # Открытие PDF файла
+        with open(file_path, 'rb') as file:
+            reader = PyPDF2.PdfReader(file)
+            return len(reader.pages)
+
+    except Exception as e:
+        logger.print(f"Error reading PDF file: {e}")
+        return None
+
+
+def extract_text_with_fitz(pdf_data: str | bytes, pages: list[int] = None):
+    # Проверяем, что за тип данных передан в pdf_data
+    if isinstance(pdf_data, bytes):
+        document = fitz.open("pdf", pdf_data)  # открываем PDF из bytes
+    else:
+        document = fitz.open(pdf_data)  # открываем PDF по пути
 
     text = ""
 
-    if only_first_page:
-        text += document.load_page(0).get_text()
-        return text
+    # Если pages не указаны, извлекаем текст со всех страниц
+    if pages is None:
+        valid_pages_to_extract = list(range(1, document.page_count + 1))
+    else:
+        valid_pages = [x + 1 for x in range(document.page_count)]
+        valid_pages_to_extract = [x for x in pages if x in valid_pages]
 
-    for page_num in range(len(document)):
-        page = document.load_page(page_num)  # загружаем страницу
+    for page_num in valid_pages_to_extract:
+        page = document.load_page(page_num - 1)  # загружаем страницу по номеру
         text += page.get_text()  # извлекаем текст
+
     return text
+
+
+def extract_pages(input_pdf_path: str, pages_to_keep: list[int], output_pdf_path: str = None) -> Optional[bytes]:
+    """ Извлечение страниц из pdf. Если output_pdf_path не задан, возвращает байты """
+
+    # Открываем исходный PDF файл
+    with open(input_pdf_path, "rb") as input_pdf_file:
+        reader = PyPDF2.PdfReader(input_pdf_file)
+        writer = PyPDF2.PdfWriter()
+        valid_pages = [x + 1 for x in range(len(reader.pages))]
+        valid_pages_to_keep = [x for x in pages_to_keep if x in valid_pages]
+
+        # Извлекаем указанные страницы
+        for page_num in valid_pages_to_keep:
+            # Нумерация страниц в PyPDF2 начинается с 0
+            writer.add_page(reader.pages[page_num - 1])
+
+        if output_pdf_path:
+            # Записываем результат в новый PDF файл
+            with open(output_pdf_path, "wb") as output_pdf_file:
+                writer.write(output_pdf_file)
+        else:
+            # Создаем байтовый буфер для хранения результата
+            output_buffer = BytesIO()
+            writer.write(output_buffer)
+
+            # Возвращаем байты PDF-файла
+            return output_buffer.getvalue()
 
 
 def is_scanned_pdf(file_path, pages_to_analyse=None):
@@ -121,7 +203,10 @@ def base64_encode_pil(image: Image.Image):
 
 # _________ IMAGES _________
 
-def image_split_top_bot(image: str | np.ndarray, top_y_shift=0.1) -> tuple[Image.Image, Image.Image]:
+def image_split_top_bot(image: str | np.ndarray, top_y_shift=0) -> tuple[Image.Image, Image.Image]:
+    if not 0 <= top_y_shift <= 1:
+        raise ValueError('Значение должно быть в диапазоне от 0 до 1')
+
     # 1, 3 -> x; 2, 4 -> y
     if isinstance(image, np.ndarray):
         pil_image = Image.fromarray(image)
