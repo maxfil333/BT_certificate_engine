534be80e13243cb8e7d9b69d1dc7e19392cf6d10
maxfil333_BT <maxfil333@gmail.com>
2024-09-26 12:06:28 +0300

    connector fix and rename

--- src/com_connector.py
+++ src/com_connector.py
@@ -2,12 +2,13 @@ import time
 import win32com.client
 
 from src.logger import logger
+from src.config import config
 
 
-def create_connection(connection_params):
+def create_connection():
     logger.print('connector initialization...')
     connection_start = time.perf_counter()
     v8com = win32com.client.Dispatch("V83.COMConnector")
-    connection = v8com.Connect(connection_params)
-    logger.print('connection time:', time.perf_counter() - connection_start)
+    connection = v8com.Connect(config['V83_CONN_STRING'])
+    logger.print(f'connection time: {time.perf_counter() - connection_start:.2f}\n')
     return connection
