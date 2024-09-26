97d119298efca7868ddc010cf9ea38fc859b9c4c
maxfil333_BT <maxfil333@gmail.com>
2024-09-26 13:01:59 +0300

    http_request both servers connection try

--- src/http_connector.py
+++ src/http_connector.py
@@ -8,24 +8,46 @@ from src.config import config
 
 
 # ______ HTTP-request ______
-def cup_http_request(function, *args, kappa=False):
+def cup_http_request(function, *args, kappa=True):
     user_1C = config['user_1C']
     password_1C = config['password_1C']
 
+    # Определение серверов
     if kappa:
-        base = r'http://kappa5.group.ru:81/ca/hs/interaction/'
+        primary_base = r'http://kappa5.group.ru:81/ca/hs/interaction/'
+        secondary_base = r'http://10.10.0.10:81/ca/hs/interaction/'
     else:
-        base = r'http://10.10.0.10:81/ca/hs/interaction/'
+        primary_base = r'http://10.10.0.10:81/ca/hs/interaction/'
+        secondary_base = r'http://kappa5.group.ru:81/ca/hs/interaction/'
 
     function_args = r'/'.join(map(lambda x: base64.urlsafe_b64encode(x.encode()).decode(), args))
-    url = base + function + r'/' + function_args
-    logger.write(url)
 
-    response = requests.get(url, auth=HTTPBasicAuth(user_1C, password_1C))
+    # Формируем URL для первого сервера
+    primary_url = primary_base + function + r'/' + function_args
+    logger.print(f"Попытка запроса: {primary_url}")
+
+    # Попытка отправить запрос на первый сервер
+    response = requests.get(primary_url, auth=HTTPBasicAuth(user_1C, password_1C))
+
+    # Если первый запрос успешен, возвращаем результат
     if response.status_code == 200:
         return response.json()
     else:
-        print(f"Ошибка: {response.status_code} - {response.reason}")
+        logger.print(f"Ошибка при запросе к первому серверу: {response.status_code} - {response.reason}")
+
+        # Формируем URL для второго сервера
+        secondary_url = secondary_base + function + r'/' + function_args
+        logger.print(f"Попытка запроса ко второму серверу: {secondary_url}")
+
+        # Попытка отправить запрос на второй сервер
+        response = requests.get(secondary_url, auth=HTTPBasicAuth(user_1C, password_1C))
+
+        # Возвращаем результат, если успешен
+        if response.status_code == 200:
+            return response.json()
+        else:
+            logger.print(f"Ошибка при запросе ко второму серверу: {response.status_code} - {response.reason}")
+            return None
 
 
 if __name__ == '__main__':
