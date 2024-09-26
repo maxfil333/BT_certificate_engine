1f6fa8e4638f961b8b015ec336d4941303cab680
maxfil333_BT <maxfil333@gmail.com>
2024-09-26 12:10:32 +0300

    refactor

--- src/http_connector.py
+++ src/http_connector.py
@@ -4,12 +4,13 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 from src.logger import logger
+from src.config import config
 
 
 # ______ HTTP-request ______
 def cup_http_request(function, *args, kappa=False):
-    user_1C = os.getenv('user_1C')
-    password_1C = os.getenv('password_1C')
+    user_1C = config['user_1C']
+    password_1C = config['password_1C']
 
     if kappa:
         base = r'http://kappa5.group.ru:81/ca/hs/interaction/'
@@ -25,3 +26,11 @@ def cup_http_request(function, *args, kappa=False):
         return response.json()
     else:
         print(f"Ошибка: {response.status_code} - {response.reason}")
+
+
+if __name__ == '__main__':
+    func_name = r'UnitDataByTransactionNumber'
+    a1 = r'ТБ-0109677'
+    a2 = r'СудноФидер,Грузополучатель'
+
+    print(cup_http_request(func_name, a1, a2))
