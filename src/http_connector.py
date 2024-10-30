774c848ec3f82584a943b114e57e268a33cb4ce6
maxfil333_BT <maxfil333@gmail.com>
2024-10-30 13:46:19 +0300

    @cache_http_requests

--- src/http_connector.py
+++ src/http_connector.py
@@ -1,8 +1,7 @@
 import base64
-
 import requests
 from requests.auth import HTTPBasicAuth
-from requests.exceptions import HTTPError
+from functools import wraps
 from typing import Union
 
 from src.logger import logger
@@ -10,6 +9,37 @@ from src.config import config
 
 
 # ______ HTTP-request ______
+
+def cache_http_requests(func):
+    """ Декоратор для кэширования запросов на основе URL """
+
+    cache = {}
+    max_cache_size = 20
+
+    @wraps(func)
+    def wrapper(function, *args, **kwargs):
+        # Формируем ключ кэша из функции + "_" + аргументы
+        function_args = r'_'.join(args)
+        url_cache_key = function + r'_' + function_args
+
+        # Проверяем, есть ли результат в кэше для данного URL
+        if url_cache_key in cache:
+            logger.print("Получение результата из кэша...")
+            return cache[url_cache_key]
+
+        # Выполняем запрос и сохраняем результат в кэше
+        result = func(function, *args, **kwargs)
+        cache[url_cache_key] = result
+
+        if len(cache) > max_cache_size:
+            cache.pop(next(iter(cache)))
+
+        return result
+
+    return wrapper
+
+
+@cache_http_requests
 def cup_http_request(function, *args, kappa=True) -> Union[list, dict, None]:
     user_1C = config['user_1C']
     password_1C = config['password_1C']
@@ -72,11 +102,9 @@ if __name__ == '__main__':
     print(2, cup_http_request(func_name, a1))  # -> list
 
     func_name = r'CustomsTransactionNumberFromBrokerDocument'
-    a1 = r'000055211/24'
     print(3, cup_http_request(func_name, ''))  # -> None (error 404)
 
     func_name = r'CustomsTransactionNumberFromBrokerDocument'
-    a1 = r'000055211/24'
     print(4, cup_http_request(func_name, 'z'))  # -> []
 
     func_name = r'CustomsTransactionNumberFromBrokerDocument'
